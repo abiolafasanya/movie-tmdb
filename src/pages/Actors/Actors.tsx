876afeb989e32a2a05283fef354b3ecbd46be81e
fastbeetech <hezekiahfasanya@gmail.com>
2023-04-19 16:59:36 +0100

    actors page

--- src/pages/Actors/Actors.tsx
+++ src/pages/Actors/Actors.tsx
@@ -3,14 +3,6 @@ import styles from './Actors.module.scss';
 import { Link } from 'react-router-dom';
 import Axios from '../../api/Axios';
 import { actorsType } from '../../utils/types';
-import ContentLoader, { Facebook } from 'react-content-loader';
-
-const MyLoader = () => <ContentLoader />;
-const MyFacebookLoader = () => (
-  <div style={{ height: '100vh' }}>
-    <Facebook />
-  </div>
-);
 
 const Actors = () => {
   const [persons, setPersons] = useState([] as actorsType[]);
@@ -42,45 +34,35 @@ const Actors = () => {
   return (
     <div className={styles.actors}>
       <h2 className={styles.title}>Popular Actor</h2>
-      <>
-        {loading ? (
-          MyFacebookLoader
-        ) : (
-          <div className={styles.boxGroup}>
-            {persons.length > 0 &&
-              persons.map((person) => (
-                <div key={person.id} className={styles.card}>
-                  <Link to={'/actor/'+person.id}>
-                    <img
-                      src={imageUrl + person.known_for[0].poster_path}
-                      alt=""
-                    />
-                    <div className={styles.cardDetail}>
-                      <h3>{person.name}</h3>
-                      <div className={styles.knownFor}>
-                        {person.known_for.map((known, index) => (
-                          <span key={known.id} className={styles.list}>
-                            {known.name
-                              ? known.name
-                              : known.title
-                              ? known.title
-                              : known.original_name
-                              ? known.original_name
-                              : known.original_title && known.original_title}
-                            {index !== person.known_for.length - 1 && ','}
-                          </span>
-                        ))}
-                      </div>
-                    </div>
-                  </Link>
+      <div className={styles.boxGroup}>
+        {persons.length > 0 &&
+          persons.map((person) => (
+            <div key={person.id} className={styles.card}>
+              <Link to={'/actor/' + person.id}>
+                <img src={imageUrl + person.known_for[0].poster_path} alt="" />
+                <div className={styles.cardDetail}>
+                  <h3>{person.name}</h3>
+                  <div className={styles.knownFor}>
+                    {person.known_for.map((known, index) => (
+                      <span key={known.id} className={styles.list}>
+                        {known.name
+                          ? known.name
+                          : known.title
+                          ? known.title
+                          : known.original_name
+                          ? known.original_name
+                          : known.original_title && known.original_title}
+                        {index !== person.known_for.length - 1 && ','}
+                      </span>
+                    ))}
+                  </div>
                 </div>
-              ))}
-          </div>
-        )}
-      </>
+              </Link>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
 
 export default Actors;
-// https://www.themoviedb.org/t/p/w300_and_h450_bestv2/bHHn3krbHyxQIWb4JbHkPlV6Uu1.jpg
